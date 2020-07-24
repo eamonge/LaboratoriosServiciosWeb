1ed38cd95f844bb070111e49f1436322f3e12628
eamonge <39276293+eamonge@users.noreply.github.com>
2020-07-24 13:37:57 -0600

    Lab03

--- Lab03/ClientApp/src/app/app.component.ts
+++ Lab03/ClientApp/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 import * as $ from 'jquery';
 
-declare const myFun: any;
+declare const LoaderXML: any;
 
 //import { } from '@angu'
 
 //function hello() {
 //  alert('Hello!!!');
 //}
+
+//declare files: any;
 
 @Component({
   selector: 'app-root',
@@ -17,60 +19,71 @@ declare const myFun: any;
 
 export class AppComponent implements OnInit {
   title = 'app';
-
-  myFunction(xml) {
-    var i;
-    var xmlDoc = xml.responseXML;
-    var table = "<tr><th>Artist</th><th>Title</th></tr>";
-    var x = xmlDoc.getElementsByTagName("CD");
-    for (i = 0; i < x.length; i++) {
-      table += "<tr><td>" +
-        x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-        x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-        "</td></tr>";
-    }
-    document.getElementById("demo").innerHTML = table;
+  files: any;
+  Load() {
+    LoaderXML();
   }
+  //myFunction(xml) {
+  //  var i;
+  //  var xmlDoc = xml.responseXML;
+  //  var table = "<tr><th>Artist</th><th>Title</th></tr>";
+  //  var x = xmlDoc.getElementsByTagName("CD");
+  //  for (i = 0; i < x.length; i++) {
+  //    table += "<tr><td>" +
+  //      x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+  //      "</td><td>" +
+  //      x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+  //      "</td></tr>";
+  //  }
+  //  document.getElementById("demo").innerHTML = table;
+  //}
 
-  loadXMLDoc() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        myFunction(this);
-      }
-    };
-    xmlhttp.open("GET", "cd_catalog.xml", true);
-    xmlhttp.send();
-  }
+  //loadXMLDoc() {
+  //  var xmlhttp = new XMLHttpRequest();
+  //  xmlhttp.onreadystatechange = function () {
+  //    if (this.readyState == 4 && this.status == 200) {
+  //      myFunction(this);
+  //    }
+  //  };
+  //  xmlhttp.open("GET", "cd_catalog.xml", true);
+  //  xmlhttp.send();
+  //}
 
   
 
   ngOnInit() {
+
+
     //
     $(".custom-file-input").on("change", function () {
       var fileName = $(this).val().toString();
       $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
     });
-    this.loadXMLDoc();
+    //this.loadXMLDoc();
     //
-  //  $(document).ready(function () {
-  //    $('#customFile').on('change', function (evt) {
-  //      var txt = document.getElementById('ShowFile');
-  //    var f = evt.target.files[0];
-  //    if (f) {
-  //      var r = new FileReader();
-  //      r.onload = function (e) {
-  //        console.log(e.target.result);
-  //        txt.textContent = e.target.result;
-  //      };
-  //      r.readAsText(f);
-  //      console.log('FUCKING SHIT');
-  //    } else {
-  //      console.log("failed");
-  //    }
-  //  });
-  //});
+
+
+
+    //var fileInput = document.getElementById('customFile');
+    //var xmlFileInfo = document.getElementById('ShowFile');
+    //var files: any;
+
+    //fileInput.addEventListener('change', function (e) {
+    //  var file = fileInput.files[0];
+    //  var textType = /text.*/;
+
+    //  if (file.type.match(textType)) {
+    //    var reader = new FileReader();
+
+    //    reader.onload = function (e) {
+    //      xmlFileInfo.innerText = reader.result;
+    //    }
+
+    //    reader.readAsText(file);
+    //  } else {
+    //    xmlFileInfo.innerText = "File not supported!"
+    //  }
+    //});
 
 
   }
--- Lab03/ClientApp/src/assets/NewFolder/custom2.js
+++ Lab03/ClientApp/src/assets/NewFolder/custom2.js
@@ -2,6 +2,21 @@
 //  alert('Hello Cust2!!!');
 //})()
 
-function myFun() {
-  window.alert('Halo');
+function LoaderXML() {
+  $(document).ready(function () {
+    $('#customFile').on('change', function (evt) {
+      var txt = document.getElementById('ShowFile');
+      var f = evt.target.files[0];
+      if (f) {
+        var r = new FileReader();
+        r.onload = function (e) {
+          console.log(e.target.result);
+          txt.textContent += e.target.result + '\n\n | \n\n';
+        };
+        r.readAsText(f);
+      } else {
+        console.log("failed");
+      }
+    });
+  });
 }
