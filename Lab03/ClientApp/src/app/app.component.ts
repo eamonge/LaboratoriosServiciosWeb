7a35e09d3378ce1d75e94efae73ad38ca48b5ab1
eamonge <39276293+eamonge@users.noreply.github.com>
2020-07-24 19:46:43 -0600

    Mejoramiento de proyecto VS

--- Lab03/ClientApp/src/app/app.component.ts
+++ Lab03/ClientApp/src/app/app.component.ts
@@ -4,14 +4,6 @@ import * as $ from 'jquery';
 
 declare const LoaderXML: any;
 
-//import { } from '@angu'
-
-//function hello() {
-//  alert('Hello!!!');
-//}
-
-//declare files: any;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -23,68 +15,13 @@ export class AppComponent implements OnInit {
   Load() {
     LoaderXML();
   }
-  //myFunction(xml) {
-  //  var i;
-  //  var xmlDoc = xml.responseXML;
-  //  var table = "<tr><th>Artist</th><th>Title</th></tr>";
-  //  var x = xmlDoc.getElementsByTagName("CD");
-  //  for (i = 0; i < x.length; i++) {
-  //    table += "<tr><td>" +
-  //      x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
-  //      "</td><td>" +
-  //      x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-  //      "</td></tr>";
-  //  }
-  //  document.getElementById("demo").innerHTML = table;
-  //}
-
-  //loadXMLDoc() {
-  //  var xmlhttp = new XMLHttpRequest();
-  //  xmlhttp.onreadystatechange = function () {
-  //    if (this.readyState == 4 && this.status == 200) {
-  //      myFunction(this);
-  //    }
-  //  };
-  //  xmlhttp.open("GET", "cd_catalog.xml", true);
-  //  xmlhttp.send();
-  //}
-
-  
 
   ngOnInit() {
 
-
-    //
     $(".custom-file-input").on("change", function () {
       var fileName = $(this).val().toString();
       $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
     });
-    //this.loadXMLDoc();
-    //
-
-
-
-    //var fileInput = document.getElementById('customFile');
-    //var xmlFileInfo = document.getElementById('ShowFile');
-    //var files: any;
-
-    //fileInput.addEventListener('change', function (e) {
-    //  var file = fileInput.files[0];
-    //  var textType = /text.*/;
-
-    //  if (file.type.match(textType)) {
-    //    var reader = new FileReader();
-
-    //    reader.onload = function (e) {
-    //      xmlFileInfo.innerText = reader.result;
-    //    }
-
-    //    reader.readAsText(file);
-    //  } else {
-    //    xmlFileInfo.innerText = "File not supported!"
-    //  }
-    //});
-
 
   }
 }
